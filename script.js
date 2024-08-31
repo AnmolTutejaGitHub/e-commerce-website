49f4d9ed5910486e6e7f980fce395db110bcc17f
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-08-31 14:25:13 +0530

    added slider

--- script.js
+++ script.js
@@ -8,3 +8,24 @@ window.addEventListener('scroll', function () {
         navbar.style.position = '';
     }
 });
+
+
+// slider
+const slider = document.querySelectorAll('.slider');
+for (let i = 0; i < slider.length; i++) {
+    slider[i].style.left = `${i * 100}%`;
+}
+
+let count = 0;
+function slideShow() {
+    for (let i = 0; i < slider.length; i++) {
+        slider[i].style.transform = `translateX(-${count * 100}%)`;
+    }
+    count++;
+    if (count == slider.length) {
+        count = 0;
+    }
+    setTimeout(slideShow, 3000);
+}
+
+slideShow();
